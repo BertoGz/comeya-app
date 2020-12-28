85979647d24597abd54255ed997f0f68b91d31b0
BertoGz <bertogzlx@gmail.com>
2020-12-28 09:00:51 -0800

    added foodCategory screen

--- MainApp/Containers/FoodCategory/index.js
+++ MainApp/Containers/FoodCategory/index.js
@@ -1,3 +1,26 @@
-const FoodCategory = () => {
-  return <Text></Text>;
+import React, { useState, useLayoutEffect } from "react";
+import { View, Text, FlatList } from "react-native";
+import { getFoodDataFromCategory } from "../../Utils/Requests";
+import Styles from "../../Utils/Styles";
+import FoodCard from "../../Components/FoodCard";
+const FoodCategory = ({ route }) => {
+  const category = route?.params?.category;
+  console.log(category);
+  const [foodData, setFoodData] = useState([]);
+  useLayoutEffect(() => {
+    getFoodDataFromCategory({
+      tag: category,
+      onSuccess: (response) => {
+        setFoodData(response);
+      },
+    });
+  }, []);
+  return (
+    <View style={{ height: "100%", width: "100%" }}>
+      <View style={{ flex: 1, ...Styles.centered }}></View>
+      <FlatList data={foodData} renderItem={FoodCard} />
+    </View>
+  );
 };
+
+export default FoodCategory;
