c723d584e6721c68ee67216b9a40f73edfe58801
BertoGz <bertogzlx@gmail.com>
2021-10-13 23:59:54 -0700

    added tabs navigation

--- MainApp/AppNavigation.js
+++ MainApp/AppNavigation.js
@@ -3,12 +3,46 @@ import React from "react";
 import Register from "./Containers/Register";
 import Home from "./Containers/Home";
 import FoodCategory from "./Containers/FoodCategory";
+import ProfileRoot from "./Containers/Profile";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Colors from "./Utils/Colors";
 const Stack = createStackNavigator();
-
+const Tab = createBottomTabNavigator();
+const tabNavigatorScreenOptions = {
+  headerShown: false,
+};
+const HomeStackScreenOptions = { headerShown: false };
+const ProfileStackScreenOptions = { headerShown: true };
 const AppNavigation = () => {
+  const HomeStack = () => {
+    return (
+      <Stack.Navigator screenOptions={HomeStackScreenOptions}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="FoodCategory" component={FoodCategory} />
+      </Stack.Navigator>
+    );
+  };
+
+  const ProfileStack = () => {
+    //Import the other screens you use!
+    return (
+      <Stack.Navigator screenOptions={ProfileStackScreenOptions}>
+        <Stack.Screen name="ProfileRoot" component={ProfileRoot} />
+      </Stack.Navigator>
+    );
+  };
+
   return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={tabNavigatorScreenOptions}>
+        <Tab.Screen name="Main" component={HomeStack} />
+        <Tab.Screen name="Profile" component={ProfileStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+  /*return (
     <Stack.Navigator
       initialRouteName="Register"
       headerMode="none"
@@ -18,7 +52,7 @@ const AppNavigation = () => {
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="FoodCategory" component={FoodCategory} />
     </Stack.Navigator>
-  );
+  );*/
 };
 
 export default AppNavigation;
