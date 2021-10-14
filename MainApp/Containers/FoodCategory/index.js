52855d9fc3fbf31f02e73963f41c1f2ce1773e24
BertoGz <bertogzlx@gmail.com>
2021-10-14 00:02:10 -0700

    added callback to item

--- MainApp/Containers/FoodCategory/index.js
+++ MainApp/Containers/FoodCategory/index.js
@@ -1,15 +1,30 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useCallback } from "react";
 import { View, Text, FlatList } from "react-native";
 import Styles from "../../Utils/Styles";
 import Colors from "../../Utils/Colors";
 import FoodCard from "../../Components/FoodCard";
 import BackgroundColor from "../../Components/BackgroundColor";
+import { useDispatch, useSelector } from "react-redux";
+import { addToFavorites } from "../../Redux/Actions";
+import { store } from "../../../App";
 const FoodCategory = ({ route }) => {
   const foodData = route?.params?.foodData;
   const category = route?.params?.category;
-
+  const { userData } = useSelector((userData) => userData);
+  const dispatch = useDispatch();
   //const [foodData, setFoodData] = useState([]);
-
+  const FoodCardCallback = useCallback(
+    ({ item }) => {
+      const { id } = item || {};
+      const favorites = userData.favorites;
+      const isLiked = favorites.has(id);
+      const onPressItem = () => {
+        dispatch(addToFavorites(id));
+      };
+      return <FoodCard {...{ item, onPressItem, isLiked }} />;
+    },
+    [userData]
+  );
   return (
     <View
       style={{
@@ -22,7 +37,8 @@ const FoodCategory = ({ route }) => {
       <FlatList
         contentContainerStyle={{}}
         data={foodData}
-        renderItem={FoodCard}
+        extraData={userData}
+        renderItem={FoodCardCallback}
         keyExtractor={(item) => item.id.toString()}
         ListHeaderComponent={() => {
           return <ListHeader {...{ category }} />;
