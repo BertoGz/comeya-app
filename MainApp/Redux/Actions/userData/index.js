5ee05388ba5edf099f4206052af2dcdaf5897c65
BertoGz <bertogzlx@gmail.com>
2021-10-14 21:08:05 -0700

    added remove from favs action

--- MainApp/Redux/Actions/userData/index.js
+++ MainApp/Redux/Actions/userData/index.js
@@ -1,6 +1,11 @@
 export const ADD_TO_FAVORITES = "ADD_TO_FAVORITES";
+export const REMOVE_FROM_FAVORITES = "REMOVE_FROM_FAVORITES";
 
-export const addToFavorites = (itemId) => ({
+export const addToFavorites = (item) => ({
   type: ADD_TO_FAVORITES,
-  payload: itemId,
+  payload: item,
+});
+export const removeFromFavorites = (item) => ({
+  type: REMOVE_FROM_FAVORITES,
+  payload: item,
 });
